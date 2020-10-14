1d69b5b53f0760ebbd149c0d438d8d7d86d6fab2
sdk <admin@sundo.kim>
2020-10-14 22:26:40 +0900

    isRequired

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent />;
+  return (
+    <MyComponent name="REACT" favoriteNumber={33}>
+      리액트
+    </MyComponent>
+  );
 };
 
 export default App;
