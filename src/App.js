627718319be4605393bcc867a250a7539fb73869
sdk <admin@sundo.kim>
2020-10-14 21:32:19 +0900

    3항 연산자

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 function App(){
-  const name = '리엑트';
+  const name = '리액트';
   return (
     <>
-    <h1>{name} 안녕!</h1>
-    <h2>잘 작동하니?</h2>
+    { name === '리액트' ? (
+        <h1>리엑트입ㄴ디ㅏ.</h1>
+    ) : (
+      <h2>리엑트가 아닙니다</h2>
+    )}
+  
     </>
   )
 }
