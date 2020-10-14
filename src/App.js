f9eb95e8e294ab75379a059af3fb65ea9b5ba8b2
sdk <admin@sundo.kim>
2020-10-14 21:26:07 +0900

    use Fragment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 function App(){
   return (
-    <div>
+    <Fragment>
     <h1>리액트 안녕!</h1>
     <h2>잘 작동하니?</h2>
-    </div>
+    </Fragment>
   )
 }
 
