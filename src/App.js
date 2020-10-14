7a89da6a77b681ed18543d1ee49f7f9b7328cacb
sdk <admin@sundo.kim>
2020-10-14 21:45:34 +0900

    inline styleing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App(){
-  const name = undefined;
-return <div>{name || '리액트'}</div>;
+  const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48',
+    fontWeight: 'bold',
+    padding: 16
+  }
+return <div style={style}>{name || '리액트'}</div>;
 }
 
 export default App;
