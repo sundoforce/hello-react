e2f2cb2156f677d0fc6622b02aae351527144492
sdk <admin@sundo.kim>
2020-10-14 21:36:24 +0900

    &&(AND) 연산자 렌더링2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App(){
   const name = '리액트';
-  return <div>{name === '리액트' ? <h1>리액트입니다</h1> : null}</div>
+  return <div>{name === '리액트' && <h1>리액트입니다</h1>}</div>
 }
 
 export default App;
