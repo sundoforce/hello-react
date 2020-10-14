88ce23f0033c304a803c8263f17cc513d06b5fc2
sdk <admin@sundo.kim>
2020-10-14 22:20:42 +0900

    childe prop

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const MyComponent = (props) => {
-  return <div>안녕하세요, 제 이름은 {props.name}입니다</div>;
+const MyComponent = ({ name, children }) => {
+  return (
+    <div>
+      안녕하세요, 제 이름은 {name}입니다 <br />
+      children 값은 {children}
+      입니다.
+    </div>
+  );
 };
 
 MyComponent.defaultProps = {
