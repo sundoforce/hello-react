978d556ac0f15b608494c714d289c7b5eab5141d
sdk <admin@sundo.kim>
2020-10-14 21:48:45 +0900

    css class

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import './App.css';
 
 function App(){
   const name = '리액트';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48',
-    fontWeight: 'bold',
-    padding: 16
-  }
-return <div style={style}>{name || '리액트'}</div>;
+
+return <div className="react">{name || '리액트'}</div>;
 }
 
 export default App;
