4651f627576795c8dab77866407c85e492df9235
sdk <admin@sundo.kim>
2020-10-14 22:43:50 +0900

    3.4.1.2 remove constructor

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초깃값 설정
-    this.state = {
-      number: 0,
-      fixedNumber: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
   render() {
     const { number, fixedNumber } = this.state;
     return (
