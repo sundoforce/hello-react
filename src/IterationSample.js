36f5402b342a21cd7bd90e04d792cb270d77ac40
sdk <admin@sundo.kim>
2020-10-15 21:48:23 +0900

    #6.4.3 useState 사용

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-    const names = ['눈사랆', '얼음', '눈', '바람']
-    const nameList = names.map((name, index) => <li key={index}>{name}</li>)
+    const [names, setNames]  = useState([
+        { id: 1, text: '눈사람'},
+        { id: 2, text: '얼금'},
+        { id: 3, text: '눈'},
+        { id: 4, text: '바랍'}
+    ]);
+    const [inputText, setInputext] = useState('');
+    const [nextId, setNextId] = useState(5);
+
+    const nameList = names.map(name => <li key={name.id}>{name.text}</li>)
     return <ul>{nameList}</ul>;
 }
 
