33fb5a9713dbb7be641520fd385fa79cfd3875ec
sdk <admin@sundo.kim>
2020-10-15 16:27:11 +0900

    EventPractice.js

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,46 +1,51 @@
 import React, { useState } from 'react';
 
-
 const EventPractice = () => {
+    const [form, setForm] = useState({
+        username: '',
+        message: ''
+    });
 
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
-    const onChangeUsername = e => setUsername(e.target.value);
-    const onChangeMessage = e => setMessage(e.target.value);
+    const { username, message} = form;
+    const onChange = e => {
+        const nextForm = {
+            ...form, // 기존 form 내용 이자리에 복사 
+            [e.target.name]: e.target.value // 원하는것 덮어 쒸우기
+        }
+        setForm(nextForm);
+    }
     const onClick = () => {
         alert(username + ': ' + message);
-        setUsername('');
-        setMessage('');
-    }
+        setForm({
+            username: '',
+            message: ''
+        });
+    };
     const onKeyPress = e => {
         if (e.key === 'Enter') {
             onClick();
         }
     };
-
-    return (
-      <div>
+    return(
+    <div>
         <h1>이벤트 연습</h1>
-        <input 
-          type="text"
-          name="username"
-          placeholder="사용자명"
-          value={username}
-          onChange={onChangeUsername}
+        <input
+            type="text"
+            name="username"
+            placeholder="사용자명"
+            value={username}
+            onChange={onChange}
         />
-        <input 
-          type="text"
-          name="message"
-          placeholder="아무거나 입력해 보세요"
-          value={message}
-          onChange={onChangeMessage}
-          onKeyPress={onKeyPress}
+        <input
+            type="text"
+            name="message"
+            placeholder="아무거나 입력해 보세요"
+            value={message}
+            onChange={onChange}
+            onKeyPress={onKeyPress}
         />
         <button onClick={onClick}>확인</button>
-      </div>
+    </div>
     );
-  };
-
-
-
+ };
 export default EventPractice;
